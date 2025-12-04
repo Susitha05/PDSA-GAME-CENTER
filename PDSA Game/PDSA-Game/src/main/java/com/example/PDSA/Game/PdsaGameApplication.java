0d02ab1f618fecd524dfe7739765b7c308c715e2
Susitha Ayodya <160872585+Susitha05@users.noreply.github.com>
2025-12-04 22:46:16 +0530

    environment setup
    e

---
+++ PDSA Game/PDSA-Game/src/main/java/com/example/PDSA/Game/PdsaGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.PDSA.Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsaGameApplication.class, args);
+	}
+
+}
---
+++ PDSA Game/PDSA-Game/src/test/java/com/example/PDSA/Game/PdsaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PDSA.Game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
