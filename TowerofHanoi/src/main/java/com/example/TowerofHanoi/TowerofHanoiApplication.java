1ed1181e82c1dc68cf4948d38b55b5d898445e2d
heshan002 <161044246+heshan002@users.noreply.github.com>
2025-12-13 12:09:01 +0530

    Minor Env changes

--- TowerofHanoi/src/main/java/com/example/TowerofHanoi/TowerofHanoiApplication.java
+++ TowerofHanoi/src/main/java/com/example/TowerofHanoi/TowerofHanoiApplication.java
@@ -1,13 +1,13 @@
-package com.towerofhanoi.demo;
+package com.example.TowerofHanoi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TowerofHanoiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TowerofHanoiApplication.class, args);
 	}
 
 }
--- TowerofHanoi/src/test/java/com/example/TowerofHanoi/TowerofHanoiApplicationTests.java
+++ TowerofHanoi/src/test/java/com/example/TowerofHanoi/TowerofHanoiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.towerofhanoi.demo;
+package com.example.TowerofHanoi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TowerofHanoiApplicationTests {
 
 	@Test
 	void contextLoads() {
