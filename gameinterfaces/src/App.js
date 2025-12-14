e14837791cdfd2405ab712de88db84cd7b9f9cdb
InduwaraJayaratna <induwarajayarathna06@gmail.com>
2025-12-14 14:14:23 +0530

    Modified UI

--- gameinterfaces/src/App.js
+++ gameinterfaces/src/App.js
@@ -1,14 +1,15 @@
 // import logo from './logo.svg';
 // import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-import MainPortal from './main_portal'
-import GameDisplay from './pages/game_display'
+import MainPortal from './main_portal';
+import EightQueensGame from './components/EightQueensGame';
+
 function App() {
   return(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPortal />}/>
-            <Route path="/gameDisplay" element={<GameDisplay />}/>
+          <Route path="/eight-queens" element={<EightQueensGame />}/>
         </Routes>
       </BrowserRouter>
   )
--- gameinterfaces/src/main_portal.jsx
+++ gameinterfaces/src/main_portal.jsx
@@ -1,34 +1,13 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import LOGO from "./assets/img/LOGO.png"
-import { motion } from "framer-motion";
-import {useNavigate} from "react-router-dom";
 
-
-function MainPortal(){
-
-    const navigate = useNavigate();
-    useEffect(() => {
-        const timer = setTimeout(()=>{
-            navigate("/gameDisplay")
-        },5000)
-    }, [navigate]);
+function mainPortal(){
     return(
         <div className="flex w-full h-screen bg-green-950 items-center">
             <div className="flex w-full h-full justify-center items-center">
-                <motion.div
-                    animate={{ scale: [1, 1.1, 1] }}
-                    transition={{
-                        duration: 2,
-                        repeat: Infinity,
-                        repeatType: "loop",
-                        ease: "easeInOut"
-                    }}
-                >
-                    <img className="w-72 h-72 object-fill" src={LOGO} alt="LOGO"/>
-                </motion.div>
+                <img className="w-72 h-72 object-fill" src={LOGO} alt="LOGO"/>
             </div>
         </div>
     )
 }
-
-export default MainPortal
+export default mainPortal
